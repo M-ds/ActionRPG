af18fc97635d26a3599ccb01f03f30c9714ff7e5
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-09-30 20:41:33 +0200

    Player - Made a basic Player Movement based on the youtube videos

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,16 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
+
 # This function is called everytime the phisics would update.
 func _physics_process(delta):
-	print("Hello World")
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if(input_vector != Vector2.ZERO):
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity)
