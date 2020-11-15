f03e83fab485ff5e1a160b86909d05ff1726b4a1
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-11-15 20:25:19 +0100

    CP 7 - TileMap - Created tilemap

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,9 @@ var velocity = Vector2.ZERO
 
 # Setting a reference to the animationPlayer function (underneath the player)
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+# Get access to the animation State, in order to toggle between idle and run
+onready var animationState = animationTree.get("parameters/playback")
 
 # This function is called everytime the physics would update.
 # Delta -> heeft de waarde die de waarde heeft van hoelang de vorige zelfde actie heeft.
@@ -15,20 +18,15 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	# this makes sure that in whatever direction the person is moving, the speed is constant in each direction
-	# think about the graph, and the circle. The circle is the normalized part.
-	input_vector = input_vector.normalized()
-	
+
 	if(input_vector != Vector2.ZERO):
-		if(input_vector.x > 0):
-			animationPlayer.play("RunRight")
-		else: 
-			animationPlayer.play("RunLeft")
+		# Only update the blend position when the player is moving
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		print(velocity)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
